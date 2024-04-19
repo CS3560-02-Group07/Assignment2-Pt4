4cbdb78ceb242cf57dbbbc73b6bf4ca677f129fe
Ethan <113479501+EthanSc-o7@users.noreply.github.com>
2024-04-18 20:35:46 -0700

    Created rough outline of GUI System

---
+++ src/main/java/shippingSubsystem/Group7Project_GUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package shippingSubsystem;
+
+/**
+ *
+ * @author ethan
+ */
+public class Group7Project_GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Delivery().setVisible(true);
+    }
+}
