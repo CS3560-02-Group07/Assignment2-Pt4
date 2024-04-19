11a140099a117b297c4d0a9f1762fc534426b0c3
Xylathoth <bill9192003@gmail.com>
2024-04-19 14:47:57 -0700

    Added 2 Manufacturers, 3 ProductItems and increased char limit of itemDescription

--- SHIPSYS.sql
+++ SHIPSYS.sql
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS ProductItem (
 	itemID INT,
     itemName VARCHAR (50),
     itemType VARCHAR (30),
-    itemDescription VARCHAR (50),
+    itemDescription VARCHAR (150),
     manufacturerID INT,
     itemStatus VARCHAR (15),
     PRIMARY KEY (itemID),
@@ -105,3 +105,10 @@ CREATE TABLE IF NOT EXISTS PickupLocation (
 );
 
 SELECT * FROM PickupLocation;
+
+INSERT INTO Manufacturer VALUES ('100001', 'Sports Inc.'),
+								('100002', 'Orange');
+
+INSERT INTO ProductItem VALUES ('300001', 'Aqua Flask 32 oz', 'water bottle', 'Stainless steel, insulated, refillable, reusable 32 oz water bottle.', '100001', 'available'), 
+								('300002', 'ePhone 20', 'smartphone', 'ePhone 20 is forged in titanium and features the groundbreaking O42 Pro chip, USB C, and the most powerful ePhone camera system ever.', '100002', 'available'),
+                                ('300003', 'PacBook Oxygen', 'laptop', 'Supercharged by the next-gen N2 chip, the redesign combines incredible performance and up to 19 hours of battery life.', '100002', 'available');
