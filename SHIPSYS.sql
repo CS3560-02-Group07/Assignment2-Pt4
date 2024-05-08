a06a05fb43e9d82aacf1018b3e1bc8d14f43b827
Jfortner0 <164428072+Jfortner0@users.noreply.github.com>
2024-05-07 23:56:28 -0700

    Update SHIPSYS.sql

--- SHIPSYS.sql
+++ SHIPSYS.sql
@@ -87,15 +87,12 @@ CREATE TABLE IF NOT EXISTS SaleLineItem (
 CREATE TABLE IF NOT EXISTS Package (
 	packageID INT AUTO_INCREMENT,
     orderNumber INT,
-	employeeID INT,
     trackingNumber INT,
     size INT,
     weight INT,
 	PRIMARY KEY (packageID),
     FOREIGN KEY (orderNumber)
-		REFERENCES Sale (orderNumber),
-	FOREIGN KEY (employeeID)
-		REFERENCES Employee (employeeID)
+		REFERENCES Sale (orderNumber)
 );
 
 CREATE TABLE IF NOT EXISTS PickupLocation (
